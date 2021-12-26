7cdd23bd1a10145f954aa7f2024d9932ea1d4215
Broyojo <dha@xoba.com>
2021-12-25 23:12:27 -0500

    added message id and more informative stickers

--- generate_data.py
+++ generate_data.py
@@ -2,12 +2,26 @@ import os
 import ast
 import pprint
 
+sticker_dict = {
+    "749054660769218631": "wumpus",
+    "751606379340365864": "mouse",
+    "816087792291282944": "dog",
+    "819128604311027752": "crack_apple",
+    "754108890559283200": "robot",
+    "781291131828699156": "bird",
+}
+
 with open("data.txt", "w") as d:
     for file in os.listdir("logs/"):
         with open(f"logs/{file}", "r") as f:
             data = ast.literal_eval(f.read())
-            # pp = pprint.PrettyPrinter(indent=4, compact=True)
+            pp = pprint.PrettyPrinter(indent=4, compact=True)
+            pp.pprint(data[1])
+            quit()
+
             for message in data:
                 if 'sticker_items' in message:
-                    d.write(
-                        f"{message['author']['discriminator']} {message['sticker_items'][0]['name']}\n")
+                    sticker = message['sticker_items'][0]['id']
+                    if sticker != "816086581509095424":  # filter out random person who used custom sticker
+                        d.write(
+                            f"{message['author']['discriminator']} {message['id']} {sticker_dict[sticker]}\n")
--- train.py
+++ train.py
@@ -13,19 +13,20 @@ y = []
 
 with open("data.txt", "r") as f:
     for line in f.readlines():
-        tag, sticker = line.split()
-        X.append([int(tag[0]), int(tag[1]), int(tag[2]), int(tag[3])])
+        tag, _, sticker = line.split()
+        X.append([int(tag[0]) / 10, int(tag[1]) / 10,
+                 int(tag[2]) / 10, int(tag[3]) / 10])
         y.append([sticker])
 
-sc = StandardScaler()
-X = sc.fit_transform(X)
+# sc = StandardScaler()
+# X = sc.fit_transform(X)
+
+X = np.array(X)
 
 print(X)
 
 ohe = OneHotEncoder()
-print(y[0])
 y = ohe.fit_transform(y).toarray()
-print(y)
 
 print(y)
 
@@ -33,8 +34,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 model = Sequential()
 model.add(Dense(4, input_dim=4, activation="relu"))
+model.add(Dense(16, activation="relu"))
 model.add(Dense(8, activation="relu"))
-model.add(Dense(4, activation="softmax"))
+model.add(Dense(len(y[0]), activation="softmax"))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam', metrics=['accuracy'])
@@ -50,4 +52,10 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
-print(model.predict([[6,9,4,2]]))
+pred = model.predict([[1/10, 0/10, 1/10, 1/10]])
+cats = ohe.categories_
+
+for k, v in enumerate(pred[0]):
+    print(f"{cats[0][k]}: {v}")
+
+print("most likely:", cats[0][np.argmax(pred[0])])
